ad42a68567b1815cf9c2b3ff6540f5157254e4b2
Alan 21 Code <alan21naci@gmail.com>
2020-08-13 01:59:29 -0300

    Agregue un boton al Login

---
+++ nacimiento-alan/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div>
+      <Button variant="text" color="secondary">
+        COMENZAR A CREAR PLAYLISTS
+      </Button>
+    </div>
+  );
+}
+
+export default App;
