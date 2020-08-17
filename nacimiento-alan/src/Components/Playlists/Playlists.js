a516fc47f5ad4d4ca65eb8219c9eeaedb8400611
Alan 21 Code <alan21naci@gmail.com>
2020-08-16 22:27:37 -0300

    Agregue una Tabla a la Playlists.

--- nacimiento-alan/src/Components/Playlists/Playlists.js
+++ nacimiento-alan/src/Components/Playlists/Playlists.js
@@ -1,24 +1,54 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
-import {
-  Box,
-  Paper,
-  InputBase,
-  IconButton,
-} from "@material-ui/core";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box, Typography, Paper, InputBase, IconButton } from "@material-ui/core";
+
+
 
 const Playlists = () => {
   return (
     <Box>
       <Box width={500} my={4} mx="auto">
         <Paper variant="outlined">
-          <IconButton type="submit" aria-label="search">
+          <IconButton type="submit">
             <SearchIcon />
           </IconButton>
           <InputBase placeholder="Buscar" />
         </Paper>
-      </Box> 
-    </Box>    
+      </Box>
+      <Box mb={-3} m={4}>
+        <TableContainer>
+          <Paper variant="outlined">
+            <Box px="50px" py="20px">
+              <Typography variant="h6" color="primary">
+                Resultados
+              </Typography>
+
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left" padding="none">
+                    Nombre
+                  </TableCell>
+                  <TableCell align="center">Artista</TableCell>
+                  <TableCell align="center">Álbum</TableCell>
+                  <TableCell align="center">Duración</TableCell>
+                  <TableCell align="center" padding="none">
+                    Agregar
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <Typography variant="h6" color="error">
+                    No hay resultados: utiliza la barra de búsqueda para
+                    encontrar canciones
+                  </Typography>
+                </TableRow>
+              </TableBody>
+            </Box>
+          </Paper>
+        </TableContainer>
+      </Box>
+    </Box>
   );
 };
 
